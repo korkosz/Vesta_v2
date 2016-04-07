b928af74eef2064b852198aafc873dbb990a621a
trybulem <marcin.trybulec@contractors.roche.com>
2016-04-07 09:27:01 +0200

    images in thumbnails

--- imports/components/drag and drop/dragAndDrop.js
+++ imports/components/drag and drop/dragAndDrop.js
@@ -8,7 +8,6 @@ export default angular.module("dragAndDrop", [])
             controller: DragAndDropCtrl,
             scope: {
                 output: '=',
-                confirmText: '@'
             }
         }
     });
@@ -39,14 +38,15 @@ function link(scope, el, attrs, ctrls) {
         if (dt) {
             let files = dt.files;
             if (files.length > 0) {
-                let edit = $('#edit');
+                let thumbnails = $('#thumbnails');
+                let imgsColection = new Map();
                 for (let i = files.length; i--;) {
                     let file = files[i];
                     let reader = new FileReader();
 
                     reader.readAsDataURL(file);
                     reader.onload = function(e) {
-                        addImageWithNewLine(edit, e.target.result)
+                        addImageWithNewLine(thumbnails, e.target.result)
                     };
 
                     Images.insert(file, function (err, fileObj) {
@@ -57,28 +57,34 @@ function link(scope, el, attrs, ctrls) {
             }
         }
         return false;
-
+        
         function addImageWithNewLine(container, uploadImgSrc) {
-            var divEdit = createEditDiv();
             var img = createImgElement(uploadImgSrc);
-            container.append(img, divEdit);
-
+            var divEdit = createContainingDiv(img);            
+            container.append(divEdit);
+            
             function createImgElement(_uploadImgSrc) {
-                var img = document.createElement('img');
+                var img = document.createElement('img');                
                 $(img).attr('src', _uploadImgSrc);
+                /*$(img).attr('name', '00' + Math.floor((Math.random() * 10) + 1));
+                $(img).addClass('img-thumbnail');
+                $(img).attr('width', '242');
+                $(img).attr('height', '200');
+                $(img).mouseenter(e => { 
+                    e.target.style.width = '100%';
+                    e.target.style.height = '100%';
+                });
+                $(img).mouseleave(e => {
+                    e.target.style.width = '304px';
+                    e.target.style.height = '236px';
+                });*/
                 return img;
             }
 
-            function createEditDiv() {
-                var div = document.createElement('div');
-                $(div).attr({
-                    name: "edit",
-                    contentEditable: "true"
-                }).css({
-                    'margin-bottom': '20px',
-                    'min-height': '50px',
-                    'border': '1px dashed red'
-                });
+            function createContainingDiv(img) {
+                var div = $(document.createElement('div'));
+                div.addClass('thumbnail col-sm-6 col-md-4');
+                div.append(img);
                 return div;
             }
         }
