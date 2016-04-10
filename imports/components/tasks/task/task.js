f5bab1b6091bf28a4f3a2ca80992cab35a485916
korkosz <chmieltrybulec@gmail.com>
2016-04-10 20:02:50 +0200

    2

---
+++ imports/components/tasks/task/task.js
@@ -0,0 +1,39 @@
+import Projects from '/imports/api/project/project';
+import Ideas from '/imports/api/ideas/idea';
+import Metadata from '/imports/api/metadata/metadata';
+
+import pill from '/imports/components/lib/pill/pill';
+import './idea.html';
+
+class IdeaCtrl {
+    constructor($scope, $routeParams, $sce) {
+        $scope.viewModel(this);
+        
+    
+        this.$routeParams = $routeParams;
+        this.helpers({
+            idea() {
+                var idea = Ideas.findOne({_id: this.$routeParams.id});
+                if(idea) {
+                    let project = Projects.findOne({_id: idea.projectId});
+                    idea.projectName = project && project.name;
+                    idea.desc = function() {
+                        return $sce.trustAsHtml(idea.description);    
+                    };
+                }
+                return idea;
+            },
+            ideaStatuses() {           
+                  console.log(Metadata.findOne({metadataName: 'IdeaStatuses'}))
+                  return Metadata.findOne({metadataName: 'IdeaStatuses'});
+            }
+        });
+    }
+};
+
+export default angular.module('idea')
+
+    .component('idea', {      
+        templateUrl: 'imports/components/ideas/idea/idea.html',
+        controller: IdeaCtrl
+    });
