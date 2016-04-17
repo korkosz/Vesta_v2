93755dbd366e96feca4d5e426c5e92922df943fb
korkosz <chmieltrybulec@gmail.com>
2016-04-17 11:20:42 +0200

    projects loaded only first time

--- server/main.js
+++ server/main.js
@@ -1,14 +1,14 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import IdeasCollection from '/imports/api/ideas/idea';
 import ProjectsCollection from '/imports/api/project/project';
 import TaskCollection from '/imports/api/task/task';
 import Metadata from '/imports/api/metadata/metadata';
 
 Images.allow({
-  'insert': function () {
-    // add custom authentication code here
-    return true;
-  }
+    'insert': function () {
+        // add custom authentication code here
+        return true;
+    }
 });
 
 Meteor.publish(null, function () {
@@ -16,41 +16,45 @@ Meteor.publish(null, function () {
 });
 
 Meteor.startup(() => {
-	ProjectsCollection.remove({});
-	    
-	ProjectsCollection.insert({
-		name: 'Vesta',
-		modules: [
-			'Ideas',
-			'Projects',
-			'Issues'
-		]	
-	});
+    //	ProjectsCollection.remove({});
 
-	var id1 = ProjectsCollection.insert({
-		name: 'ProjectTest1',
-		modules: [
-			'ProjectTest1_module1',
-			'ProjectTest1_module2',
-			'ProjectTest1_module3'
-		]	
-	});
+    const length = ProjectsCollection.find().count();
+    if (length < 0) {
+        ProjectsCollection.insert({
+            name: 'Vesta'
+            , modules: [
+			'Ideas'
+			, 'Projects'
+			, 'Issues'
+		]
+        });
 
-	var id2 = ProjectsCollection.insert({
-		name: 'ProjectTest2',
-		modules: [
-			'ProjectTest2_module1',
-			'ProjectTest2_module2',
-			'ProjectTest2_module3'
-		]	
-	});
+        var id1 = ProjectsCollection.insert({
+            name: 'ProjectTest1'
+            , modules: [
+			'ProjectTest1_module1'
+			, 'ProjectTest1_module2'
+			, 'ProjectTest1_module3'
+		]
+        });
 
-	var id3 = ProjectsCollection.insert({
-		name: 'ProjectTest3',
-		modules: [
-			'ProjectTest3_module1',
-			'ProjectTest3_module2',
-			'ProjectTest3_module3'
-		]	
-	});	
+        var id2 = ProjectsCollection.insert({
+            name: 'ProjectTest2'
+            , modules: [
+			'ProjectTest2_module1'
+			, 'ProjectTest2_module2'
+			, 'ProjectTest2_module3'
+		]
+        });
+
+        var id3 = ProjectsCollection.insert({
+            name: 'ProjectTest3'
+            , modules: [
+			'ProjectTest3_module1'
+			, 'ProjectTest3_module2'
+			, 'ProjectTest3_module3'
+		]
+        });
+    }
+
 });
