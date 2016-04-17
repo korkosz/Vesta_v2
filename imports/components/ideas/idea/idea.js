bb84ca72b8b4245c851060122798817f6fcb8df9
korkosz <chmieltrybulec@gmail.com>
2016-04-17 20:44:51 +0200

    reviews

--- imports/components/ideas/idea/idea.js
+++ imports/components/ideas/idea/idea.js
@@ -8,21 +8,21 @@ import './idea.html';
 class IdeaCtrl {
     constructor($scope, $routeParams, $sce) {
         $scope.viewModel(this);
-        
+
         this.review = {
             merits: [],
             drawbacks: [],
             comment: ''
         };
-        
+
         this.$routeParams = $routeParams;
-        
+
         this.helpers({
             idea() {
-                var idea = Ideas.findOne({_id: this.$routeParams.id});
-                if(idea) {
-                    let project = Projects.findOne({_id: idea.projectId});
-                    let user = Meteor.users.findOne({_id: idea.createdBy});
+                var idea = Ideas.findOne({ _id: this.$routeParams.id });
+                if (idea) {
+                    let project = Projects.findOne({ _id: idea.projectId });
+                    let user = Meteor.users.findOne({ _id: idea.createdBy });
                     let reviewers = Meteor.users.find({
                         _id: {
                             $in: idea.reviewers
@@ -30,72 +30,75 @@ class IdeaCtrl {
                     }).fetch();
                     idea.projectName = project && project.name;
                     idea.createdBy = user.profile.fullname;
-                    idea.reviewers = reviewers; 
-                    idea.desc = function() {
-                        return $sce.trustAsHtml(idea.description);    
+                    idea.reviewers = reviewers;
+                    idea.desc = function () {
+                        return $sce.trustAsHtml(idea.description);
                     };
-                    if(!idea.reviews) idea.reviews = [];                    
+                    if (!idea.reviews) idea.reviews = [];
                 }
                 return idea;
             },
-            ideaStatuses() {                                 
-                return Metadata.findOne({metadataName: 'IdeaStatuses'});
+            ideaStatuses() {
+                return Metadata.findOne({ metadataName: 'IdeaStatuses' });
             },
             reviews() {
-                var idea = Ideas.findOne({_id: this.$routeParams.id});
-                if(idea) return Reviews.find({_id: {$in: idea.reviews}});                
+                var idea = Ideas.findOne({ _id: this.$routeParams.id });
+                if (idea) return Reviews.find({ _id: { $in: idea.reviews } });
             }
         });
-    }   
-    
-    removeReview(_revId) {          
+    }
+
+    removeReview(_revId) {
         Reviews.remove(_revId, this.idea._id);
     }
     
+    removeReviewVisible(_createdBy) {
+        return Meteor.userId() === _createdBy;
+    }
+
     newReviewVisible() {
-        if(!this.idea || !Meteor.user()) return false;
-        
+        if (!this.idea || !this.reviews ||!Meteor.user()) return false;
+
         const vm = this;
-        
+
         return userInReviewers() && userDidntReviewedYet();
-        
+
         ///
-        function userInReviewers() {            
-            return vm.idea.reviewers.findIndex((_rev)=>
-                _rev._id === Meteor.userId()) > -1;        
+        function userInReviewers() {
+            return vm.idea.reviewers.findIndex((_rev) =>
+                _rev._id === Meteor.userId()) > -1;
         };
-        
+
         function userDidntReviewedYet() {
-            return vm.idea.reviews.findIndex((_rev)=>
-                _rev.createdBy === Meteor.userId()) === -1;            
+            return vm.reviews.findIndex((_rev) =>
+                _rev._createdBy === Meteor.userId()) === -1;
         };
     }
-    
+
     currentUserName() {
-        if(Meteor.user()) return Meteor.user().profile.fullname;
+        if (Meteor.user()) return Meteor.user().profile.fullname;
     }
-    
-    addReview() {          
+
+    addReview() {
         this.review._createdBy = Meteor.userId();
         this.review._ideaId = this.idea._id;
-        Reviews.insert(this.review, ()=> alert());
-        
+        Reviews.insert(this.review, () => alert());
+
         clearArray(this.review.merits);
         clearArray(this.review.drawbacks);
-        this.review.comment = '';      
-        
+        this.review.comment = '';
+
         ///
         function clearArray(arr) {
-            for(var i=0, len=arr.length; i<len;i++)
+            for (var i = 0, len = arr.length; i < len; i++)
                 arr.pop();
             return arr;
         }
-    }       
+    }
 };
 
 export default angular.module('idea')
-
-    .component('idea', {      
+    .component('idea', {
         templateUrl: 'imports/components/ideas/idea/idea.html',
         controller: IdeaCtrl
     });
