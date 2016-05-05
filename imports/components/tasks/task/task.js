e14094f501c9ffbe2a54743729596f38267460f3
korkosz <chmieltrybulec@gmail.com>
2016-05-05 20:55:46 +0200

    task - mozliwosc edycji typu i priorytetu

--- imports/components/tasks/task/task.js
+++ imports/components/tasks/task/task.js
@@ -24,15 +24,20 @@ class IdeaCtrl {
             taskStatuses() {           
                   console.log(Metadata.findOne({metadataName: 'TaskStatuses'}))
                   return Metadata.findOne({metadataName: 'TaskStatuses'});
+            }, 
+            taskTypes() {
+                return Metadata.findOne({ metadataName: 'TaskType' });
+            },
+            taskPriorities() {
+                return Metadata.findOne({ metadataName: 'TaskPriority' });
             }
         });
-    }
-    
-    setStatus() {
+    }    
+    selectListChanged(property) {
+        var updateObj = {};
+        updateObj[property] = this.task[property];
         Tasks.update(this.task._id, {
-            $set: {
-                status: this.task.status
-            }
+            $set: updateObj
         });          
     }
 };
