4bac776b65c7d2e4659d4cc3ccfb1cfd94de7962
korkosz <chmieltrybulec@gmail.com>
2016-05-07 18:12:07 +0200

    wysiwyg

--- imports/components/tasks/task new/task_new.js
+++ imports/components/tasks/task new/task_new.js
@@ -14,7 +14,7 @@ function NewTaskCtrl($scope) {
 
     vm.task = {};
     vm.task.description = '';
-    
+
     // KURWA DODAJ JEBANY ID DO JEBANEGO KURWA MODULU
     $scope.$watch('vm.project', function () {
         if (vm.project) {
@@ -64,7 +64,8 @@ function NewTaskCtrl($scope) {
 NewTaskCtrl.$inject = ['$scope'];
 
 export default angular.module("task")
-    .directive('newTask', ['$q', function ($q) {
+    .directive('newTask', ['$q', 'Upload', 'cloudinary', function (
+        $q, Upload, cloudinary) {
         return {
             templateUrl: "imports/components/tasks/task new/task_new.html",
             controller: NewTaskCtrl,
@@ -81,53 +82,57 @@ export default angular.module("task")
         function link(scope, el, attrs, ctrl) {
             if (!ctrl.title) ctrl.title = 'New Task';
 
+            function uploadToServer(file, def) {
+                file.upload = Upload.upload({
+                    url: "https://api.cloudinary.com/v1_1/" + 
+                        cloudinary.config().cloud_name + "/upload",
+                    data: {
+                        upload_preset: cloudinary.config().upload_preset,
+                        tags: 'myphotoalbum',
+                        file: file
+                    }
+                }).success(function (data, status, headers, config) {
+                    var img = $('#' + file.name).attr('src', data.url);
+                    def.resolve();
+                }).error(function (data, status, headers, config) {
+                    console.error('Sth went wrong when uploading image');
+                });
+            };
+
+            function dataURItoBlob(dataURI) {
+                var binary = atob(dataURI.split(',')[1]);
+                var array = [];
+                for (var i = 0; i < binary.length; i++) {
+                    array.push(binary.charCodeAt(i));
+                }
+                return new Blob([new Uint8Array(array)], { type: 'image/jpeg' });
+            }
+
             ctrl.compileOutput = function () {
-                var editEl = el.find('#edit');
+                var defer = $q.defer();
+                var promises = [];
+                var counter = 0;
+                var editEl = $('new-task div[id^="taTextElement"]');
                 var imgs = editEl.find('img');
                 var imgsLen = imgs.length;
-                var promises = [];
 
                 while (imgsLen--) {
                     let img = imgs.eq(imgsLen);
-                    let file = img.data('file');
-                    let def = $q.defer();
-                    let promise = def.promise;
+                    let src = img.attr('src');
+                    let blob = dataURItoBlob(src);
+                    //name is UTC timestamp in miliseconds
+                    let name = Date.now() + '';
+                    let file = new File([blob], name);
+                    img.attr('id', name);
+                    let defer = $q.defer();
+                    let promise = defer.promise;
                     promises.push(promise);
                     if (file) {
-                        Images.insert(file, function (err, fileObj) {
-                            file.id = fileObj._id;
-                        });
-
-                        let stop = setInterval(() => {
-                            var _imgDb = Images.findOne({
-                                _id: file.id
-                            });
-                            if (_imgDb) {
-                                if (_imgDb.url()) {
-                                    img.attr('src', _imgDb.url());
-                                    scope.$apply(function () {
-                                        removeEditableAttr();
-                                        def.resolve();
-                                    });
-                                    clearInterval(stop);
-                                }
-                            }
-                        }, 1000);
+                        uploadToServer(file, defer);
                     }
                 }
-
-                function removeEditableAttr() {
-                    var divs = $("div[name='edit']");
-                    var len = divs.length;
-                    while (len--) {
-                        let div = divs.eq(len);
-                        div.removeAttr('contentEditable');
-                        div.css('border', 'none');
-                    }
-                }
-
-                return $q.all(promises).then(function () {
-                    ctrl.task.description = editEl.html();
+                return $q.all(promises).then(() => {
+                    this.task.description = editEl.html();
                 });
             };
         }
--- imports/components/tasks/task/task.js
+++ imports/components/tasks/task/task.js
@@ -8,23 +8,24 @@ import './task.html';
 class IdeaCtrl {
     constructor($scope, $routeParams, $sce) {
         $scope.viewModel(this);
-        
-    
+
+        this.descriptionEdited = false;
         this.$routeParams = $routeParams;
+        
         this.helpers({
             task() {
-                var task = Tasks.findOne({_id: this.$routeParams.id});
-                if(task) {
-                    task.desc = function() {
-                        return $sce.trustAsHtml(task.description);    
+                var task = Tasks.findOne({ _id: this.$routeParams.id });
+                if (task) {
+                    task.desc = function () {
+                        return $sce.trustAsHtml(task.description);
                     };
                 }
                 return task;
             },
-            taskStatuses() {           
-                  console.log(Metadata.findOne({metadataName: 'TaskStatuses'}))
-                  return Metadata.findOne({metadataName: 'TaskStatuses'});
-            }, 
+            taskStatuses() {
+                console.log(Metadata.findOne({ metadataName: 'TaskStatuses' }))
+                return Metadata.findOne({ metadataName: 'TaskStatuses' });
+            },
             taskTypes() {
                 return Metadata.findOne({ metadataName: 'TaskType' });
             },
@@ -32,20 +33,58 @@ class IdeaCtrl {
                 return Metadata.findOne({ metadataName: 'TaskPriority' });
             }
         });
-    }    
+    }
     selectListChanged(property) {
         var updateObj = {};
         updateObj[property] = this.task[property];
         Tasks.update(this.task._id, {
             $set: updateObj
-        });          
+        });
     }
 };
 IdeaCtrl.$inject = ['$scope', '$routeParams', '$sce'];
 
 export default angular.module('task')
 
-    .component('task', {      
-        templateUrl: 'imports/components/tasks/task/task.html',
-        controller: IdeaCtrl
+    .directive('task', function () {
+        return {
+            templateUrl: 'imports/components/tasks/task/task.html',
+            controller: IdeaCtrl,
+            controllerAs: "$ctrl",
+            link
+        }
     });
+function link(scope, el, attr, ctrl) {
+    // hide toolbar
+    el.find('[text-angular-toolbar]').css('display', 'none');
+    var editor = el.find('.ta-editor');
+
+    // activate and diactivate edition
+    editor.dblclick(function () {
+        el.find('[text-angular-toolbar]').css('display', 'block');
+        ctrl.descriptionEdited = true;
+    });
+    editor.focusout(function () {
+        el.find('[text-angular-toolbar]').css('display', 'none');
+        ctrl.descriptionEdited = false;
+    });
+
+    // setTimeout(function () {
+    //     // if you click 2x on img modal will apear 
+    //     var imgs = editor.find('img');
+    //     imgsLen = imgs.length;
+
+    //     while (imgsLen--) {
+    //         let img = imgs.eq(imgsLen);
+    //         img.click(function (event) {
+    //             // if description is not in edit mode right now
+    //             if (!ctrl.descriptionEdited) {
+    //                 event.preventDefault();
+    //                 event.stopPropagation();
+    //                 imgModalBody.append(img);
+    //                 imgModal.modal();
+    //             }
+    //         });
+    //     }
+    // }, 1000);
+}
