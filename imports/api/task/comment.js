d28e36ef510f63730b5aff9b53b7c2e98af9984e
korkosz <chmieltrybulec@gmail.com>
2016-05-11 08:02:53 +0200

    bug

---
+++ imports/api/task/comment.js
@@ -0,0 +1,36 @@
+import { Mongo } from 'meteor/mongo';
+import Tasks from '/imports/api/task/task';
+
+class CommentsCollection extends Mongo.Collection {
+    insert(comment, callback) {
+        super.insert(comment, function (err, res) {
+            Tasks.update(comment.taskId, {
+                $push: {
+                    comments: res
+                }
+            });
+        });
+    }
+}
+
+export default Comments = new CommentsCollection('Tasks.Comments');
+
+CommentsCollection.schema = new SimpleSchema({
+    content: {
+        type: String
+    },
+    taskId: {
+        type: String
+    },
+    createdBy: {
+        type: String,
+        defaultValue: this.userId,
+        optional: true
+    },
+    createdAt: {
+        type: Date,
+        optional: true
+    }
+});
+
+Comments.attachSchema(CommentsCollection.schema);
