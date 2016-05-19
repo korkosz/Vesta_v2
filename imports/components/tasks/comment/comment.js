b71e33182e41a6b7cd40d08749141f5493c9a63b
korkosz <chmieltrybulec@gmail.com>
2016-05-19 21:59:35 +0200

    comments

--- imports/components/tasks/comment/comment.js
+++ imports/components/tasks/comment/comment.js
@@ -1,8 +1,27 @@
 import './comment.html';
+import Comments from '/imports/api/task/comment';
 
 class controller {
     constructor() {
         this.moment = moment;
+        this.edited = false;
+    }
+
+    edit() {
+        this.edited = true;
+    }
+
+    save() {        
+        Comments.update(this.comment._id, {
+            $set: {
+                content: this.comment.content
+            }
+        });
+        this.edited = false;
+    }
+    
+    remove() {
+        Comments.remove(this.comment._id, 1);        
     }
 }
 
@@ -10,7 +29,7 @@ export default angular.module('task')
     .component('comment', {
         templateUrl: 'imports/components/tasks/comment/comment.html',
         bindings: {
-            comment: '='
+            comment: '<'
         },
         controller
     });
